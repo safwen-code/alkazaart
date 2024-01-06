aa84813fde57572fc67a92f84182d157b42267ee
safwen-code <safwendjebbi1234@gmail.com>
2024-01-06 17:22:22 +0100

    updated

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,7 @@ import express from 'express'
 import colors from 'colors'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import path from 'path'
-
+import cors from 'cors'
 import userRoutes from './routes/userRoutes.js'
 import shipRoutes from './routes/shipRoutes.js'
 import fornisseurRoutes from './routes/fornisseurRoutes.js'
@@ -14,24 +13,15 @@ const app = express()
 //send body
 app.use(express.json())
 
+// Enable CORS for all routes
+app.use(cors())
+
 connectDB()
 
 //use Created routes
 app.use('/api/users', userRoutes)
 app.use('/api/ship', shipRoutes)
 app.use('/api/fornisseur', fornisseurRoutes)
-
-// const __dirname = path.resolve()
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '/client/build')))
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//   })
-// } else {
-//   app.get('/test', (req, res) => {
-//     res.send('API running')
-//   })
-// }
 
 //create port
 const port = process.env.PORT || 5000
